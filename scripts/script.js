242334d864e760b260f295f2c7495117471b07b9
David San Antonio <David@Davids-MacBook-Pro.local>
2016-09-14 23:31:29 -0400

    Removed last commit.

--- scripts/script.js
+++ scripts/script.js
@@ -145,16 +145,11 @@ function listUpcomingEvents() {
 
           appendPre(where, id);
         }
-        startRefreshTimer();
       } else {
         appendPre('No upcoming events found.', id);
       }
     });
   }
-}
-
-function startRefreshTimer() {
-  setTimeout(listUpcomingEvents, 3000);
 }
 
 /**

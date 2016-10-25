76a27f0c2132ab7a9995a51f27d84a3cf04cc612
David San Antonio <David@w184-185027.wifi2184.unh.edu>
2016-10-25 14:48:55 -0400

    Added the fast money round

--- chains/script.js
+++ chains/script.js
@@ -1,9 +1,16 @@
 var app = angular.module("chain", []);
 
-app.controller("myCtrl", ['$scope', function ($scope) {
+app.controller("myCtrl", ['$scope', '$interval', function ($scope, $interval) {
     $scope.teamOneScore = 0;
     $scope.teamTwoScore = 0;
     $scope.score = 0;
+
+    $scope.total = 0;
+    $scope.timerStarted = false;
+    $scope.time = 10;
+
+    $scope.fastMoneyWords = [];
+    $scope.currentWord = 0;
 
     $scope.addTeamOneScore = function() {
         $scope.teamOneScore += $scope.score;
@@ -11,5 +18,49 @@ app.controller("myCtrl", ['$scope', function ($scope) {
 
     $scope.addTeamTwoScore = function() {
         $scope.teamTwoScore += $scope.score;
+    }
+
+    var stop;
+    $scope.startTimer = function() {
+        $scope.total = 0;
+        if ($scope.time == 0) $scope.time = 60;
+        $scope.timerStarted = true;
+
+        stop = $interval(function() {
+            if ($scope.time > 0 && $scope.total < 7) {
+                $scope.time--;
+            } else {
+                $scope.stopTimer();
+            }
+        }, 1000);
+    }
+
+    $scope.newWord = function(correct) {
+        $scope.currentWord++;
+        if (correct && $scope.total < 7) {
+            $scope.total++;
+        }
+    }
+
+    $scope.stopTimer = function() {
+        if (angular.isDefined(stop)) {
+            $interval.cancel(stop);
+            stop = undefined;
+            $scope.timerStarted = false;
+            $scope.time = 0;
+            $scope.fastMoneyWords = [];
+            $scope.currentWord = 0;
+        }
+    }
+
+    $scope.addWord = function() {
+        $scope.fastMoneyWords.push($scope.wordToAdd);
+        $scope.wordToAdd = "";
+        if ($scope.fastMoneyWords.length == 7) {
+            $scope.enterWords = false;
+            $scope.startTimer();
+        } else {
+            document.getElementById("addWords").focus();
+        }
     }
 }]);

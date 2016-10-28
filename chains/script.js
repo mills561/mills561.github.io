5d8411f5c75128c19dc4b50ca692aca3df10410c
David San Antonio <David@Davids-MacBook-Pro.local>
2016-10-28 02:42:48 -0400

    Added reset button for main chain reaction game. Also removed randomness from fast money round. Alsoat the end of a fast money round, show correct/incorrect words.

--- chains/script.js
+++ chains/script.js
@@ -7,10 +7,14 @@ app.controller("myCtrl", ['$scope', '$interval', function ($scope, $interval) {
 
     $scope.total = 0;
     $scope.timerStarted = false;
-    $scope.time = 60;
+    $scope.time = 10;
 
     $scope.fastMoneyWords = [];
+    $scope.endingWords = [];
+    $scope.correctWords = [];
     $scope.currentWord = 0;
+
+    $scope.finished = false;
 
     $scope.addTeamOneScore = function() {
         $scope.teamOneScore += $scope.score;
@@ -22,8 +26,10 @@ app.controller("myCtrl", ['$scope', '$interval', function ($scope, $interval) {
 
     var stop;
     $scope.startTimer = function() {
+        $scope.finished = false;
+        $scope.endingWords = $scope.fastMoneyWords.slice();
         $scope.total = 0;
-        $scope.currentWord = Math.floor(Math.random() * $scope.fastMoneyWords.length);
+        $scope.currentWord = 0;
         if ($scope.time == 0) $scope.time = 60;
         $scope.timerStarted = true;
 
@@ -39,9 +45,21 @@ app.controller("myCtrl", ['$scope', '$interval', function ($scope, $interval) {
     $scope.newWord = function(correct) {
         if (correct && $scope.total < 7) {
             $scope.total++;
+            $scope.correctWords.push($scope.fastMoneyWords[$scope.currentWord]);
             $scope.fastMoneyWords.splice($scope.fastMoneyWords.indexOf($scope.fastMoneyWords[$scope.currentWord]), 1);
+            if ($scope.currentWord >= $scope.fastMoneyWords.length) $scope.currentWord = 0;
+        } else {
+            if ($scope.currentWord >= $scope.fastMoneyWords.length - 1) $scope.currentWord = 0;
+            else $scope.currentWord++;
         }
-        $scope.currentWord = Math.floor(Math.random() * $scope.fastMoneyWords.length);
+    }
+
+    $scope.enterNewWords = function() {
+        $scope.correctWords = [];
+        $scope.endingWords = [];
+        if (!$scope.enterWords) $scope.fastMoneyWords = [];
+        $scope.enterWords = true; 
+        $scope.finished = false;
     }
 
     $scope.stopTimer = function() {
@@ -50,9 +68,19 @@ app.controller("myCtrl", ['$scope', '$interval', function ($scope, $interval) {
             stop = undefined;
             $scope.timerStarted = false;
             $scope.time = 0;
-            $scope.fastMoneyWords = [];
             $scope.currentWord = 0;
+            $scope.finished = true;
         }
+    }
+
+    $scope.resetWords = function() {
+        $scope.word0 = "";
+        $scope.word1 = "";
+        $scope.word2 = "";
+        $scope.word3 = "";
+        $scope.word4 = "";
+        $scope.word5 = "";
+        $scope.word6 = "";
     }
 
     $scope.addWord = function(key) {
